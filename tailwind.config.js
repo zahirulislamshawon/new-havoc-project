2abe7247053f1e5f3875e978a1fadf584a82c979
zahirulislamshawon <zahirulislamshawon@gmail.com>
2023-06-09 15:24:42 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Racing': ['Racing Sans One', 'cursive'],
+        'Inter': ['Inter', 'sans-serif'],
+
+
+      },
+    },
+  },
+  plugins: [],
+}
